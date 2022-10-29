351c0113fa7d260962268083ecb48fb160f0ad23
anemadhon <anemadhon@gmail.com>
2022-10-29 14:33:48 +0700

    add profiles endpoint

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('/profiles', \App\Http\Controllers\ApplicationController::class)->scoped([
+    'application' => 'code'
+])->except('destroy');
