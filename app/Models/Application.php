354c9e5eb4977c956cbec756da85f6e6fa12c13d
anemadhon <anemadhon@gmail.com>
2022-10-29 09:47:25 +0700

    modify model

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Application extends Model
 {
     use HasFactory;
+
+    const CREATED_AT = 'applied_at';
+    const UPDATED_AT = false;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'available_job_id',
+        'candidate_id',
+        'code', 'applied_at'
+    ];
+
+    protected $cast = [
+        'applied_at' => 'timestamp'
+    ];
+
+    public function availableJob()
+    {
+        return $this->belongsTo(AvailableJob::class);
+    }
 }
